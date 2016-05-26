ab77cd53c2987cfde36a48def24446d6ee655b84
Gian Carlo I. Gilos <gianCarlo.gilos@outlook.com>
2016-05-26 21:15:40 +0800

    We replaced standard foreach loop for getAllEmployeesOfDepartment with Streams

--- test/com/outlook/giancarlo/usecasetests/PayrollRepository.java
+++ test/com/outlook/giancarlo/usecasetests/PayrollRepository.java
@@ -15,38 +15,33 @@ import java.util.Map;
  * @author pofay
  */
 public class PayrollRepository {
-    
+
     private static final Map<Integer, Employee> employees = new HashMap<>();
     private static final Map<Integer, Department> departments = new HashMap<>();
-    
+
     public static Employee getEmployee(int empId) {
         return employees.get(empId);
     }
-    
+
     public static Department getDepartment(int deptId) {
         return departments.get(deptId);
     }
-    
+
     public static void add(Employee e) {
         employees.put(e.getId(), e);
     }
-    
+
     public static void add(Department d) {
         departments.put(d.getId(), d);
     }
-    
+
     public static void addEmployeeToDepartment(int deptId, Employee e) {
         Department d = getDepartment(deptId);
         e.setDepartment(d);
     }
-    
+
     public static List<Employee> getAllEmployeesOfDepartment(Department d) {
         List<Employee> queriedEmployees = new ArrayList<>();
-//        for (Employee e : employees.values()) {
-//            if (e.getDepartmentId() == d.getId()) {
-//                queriedEmployees.add(e);
-//            }
-//        }
         employees
                 .values()
                 .stream()
@@ -54,5 +49,5 @@ public class PayrollRepository {
                 .forEach(e -> queriedEmployees.add(e));
         return queriedEmployees;
     }
-    
+
 }
