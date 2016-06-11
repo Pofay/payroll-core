acbf126222342fab6906f48a03c2c033f975d023
Gian Carlo I. Gilos <gianCarlo.gilos@outlook.com>
2016-06-11 21:34:33 +0800

    We properly lined up the contexts in the test

--- src/test/java/com/outlook/giancarlo/usecasetests/PayrollTest.java
+++ src/test/java/com/outlook/giancarlo/usecasetests/PayrollTest.java
@@ -17,35 +17,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import org.junit.Ignore;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 /**
  *
@@ -111,129 +82,132 @@ public class PayrollTest {
                 String expectedMessage = "Department Id should be a positive number";
                 assertThat(e.getMessage(), is(equalTo(expectedMessage.toUpperCase())));
             }
-        }
 
-        public class NullObjectContext {
-
-            @Test
-            public void ItShouldReturnAnUnknownEmployeeWhenQueryingForNonExistingEmployeeInRepository() {
-                int nonExistingEmpId = 5;
-                Employee e = repository.getEmployeeById(nonExistingEmpId);
-
-                assertNotNull(e);
-                assertThat(e.getId(), is(0));
-                assertThat(e.getName(), is("Unknown Unknown"));
-                assertThat(e.getDeptId(), is(0));
-            }
-        }
-
-        public class QueryEmployees {
-
-            int empId1 = 2;
-            int empId2 = 4;
-            int empId3 = 5;
-            int deptId1 = 3;
-            int deptId2 = 2;
-
-            @Before
-            public void beforeEach() {
-                executeEmployeeCreation(empId1, deptId1, new EmployeeName("Gian Carlo", "Gilos"));
-                executeEmployeeCreation(empId2, deptId1, new EmployeeName("Raul", "Watson"));
-                executeEmployeeCreation(empId3, deptId2, new EmployeeName("Ulric", "Tristan"));
-            }
-
-            @Test
-            public void ItShouldBeAbleToGetAllEmployeesWithASpecificDepartmentId() {
-                Employee e1 = repository.getEmployeeById(empId1);
-                Employee e2 = repository.getEmployeeById(empId2);
-                List<Employee> expected = Arrays.asList(e1, e2);
-
-                List<Employee> employees = repository.getAllEmployeesWithDepartmentIdOf(deptId1);
-                assertThat(employees, is(equalTo(expected)));
-            }
-
-            @Test
-            public void ItShouldBeAbleToGetAllEmployeesWithAnotherDepartmentId() {
-                Employee e1 = repository.getEmployeeById(empId3);
-                List<Employee> expected = Arrays.asList(e1);
-
-                List<Employee> employees = repository.getAllEmployeesWithDepartmentIdOf(deptId2);
-                assertThat(employees, is(equalTo(expected)));
-            }
-
-            @Test
-            public void ItShouldBeAbleToGetAllEmployees() {
-                Employee e1 = repository.getEmployeeById(empId1);
-                Employee e2 = repository.getEmployeeById(empId2);
-                Employee e3 = repository.getEmployeeById(empId3);
-                List<Employee> expected = new ArrayList<>();
-                expected.add(e1);
-                expected.add(e2);
-                expected.add(e3);
-
-                List<Employee> employees = repository.getAllEmployees();
-
-                assertThat(employees, is(equalTo(expected)));
-            }
-        }
-
-        public class ChangeEmployeeContext {
-
-            int empId = 6;
-            int deptId = 7;
-            String firstName = "Gian Carlo";
-            String lastName = "Gilos";
-
-            @Before
-            public void beforeEach() {
-                executeEmployeeCreation(empId, deptId, new EmployeeName(firstName, lastName));
-            }
-
-            @Test
-            public void ItShouldBeAbleToChangeTheFirstNameOfAnEmployee() {
-                String changeFirstName = "Pofay";
-                ChangeEmployee ce = new ChangeEmployeeName(repository, empId,
-                        new EmployeeName(changeFirstName, lastName));
-
-                ce.execute();
-
-                String expected = "Pofay Gilos";
-                Employee e = repository.getEmployeeById(empId);
-                assertThat(e.getName(), is(equalTo(expected)));
-            }
-
-            @Test
-            public void ItShouldBeAbleToChangeTheDepartmentIdOfAnEmployee() {
-                executeEmployeeCreation(empId, deptId, new EmployeeName(firstName, lastName));
-
-                int changedDeptId = 9;
-                ChangeEmployee ce = new ChangeEmployeeDepartmentId(repository,
-                        empId, changedDeptId);
-
-                ce.execute();
-
-                Employee e = repository.getEmployeeById(empId);
-                assertThat(e.getDeptId(), is(equalTo(changedDeptId)));
-            }
-
-            @Test
-            public void ItShouldBeAbleToChangeTheLastNameOfAnEmployee() {
-                executeEmployeeCreation(empId, deptId, new EmployeeName(firstName, lastName));
-
-                String changedLastName = "Tumulak";
-                ChangeEmployee ce = new ChangeEmployeeName(repository, empId,
-                        new EmployeeName(firstName, changedLastName));
-
-                ce.execute();
-
-                Employee e = repository.getEmployeeById(empId);
-                assertThat(e.getName(), is("Gian Carlo Tumulak"));
-            }
-        }
-
-        private void executeEmployeeCreation(final int empId, int deptId, EmployeeName name) {
-            CreateEmployee ce = new CreateEmployee(repository, empId, deptId, name);
-            ce.execute();
         }
     }
+
+    public class NullObjectContext {
+
+        @Test
+        public void ItShouldReturnAnUnknownEmployeeWhenQueryingForNonExistingEmployeeInRepository() {
+            int nonExistingEmpId = 5;
+            Employee e = repository.getEmployeeById(nonExistingEmpId);
+
+            assertNotNull(e);
+            assertThat(e.getId(), is(0));
+            assertThat(e.getName(), is("Unknown Unknown"));
+            assertThat(e.getDeptId(), is(0));
+        }
+    }
+
+    public class QueryEmployees {
+
+        int empId1 = 2;
+        int empId2 = 4;
+        int empId3 = 5;
+        int deptId1 = 3;
+        int deptId2 = 2;
+
+        @Before
+        public void beforeEach() {
+            executeEmployeeCreation(empId1, deptId1, new EmployeeName("Gian Carlo", "Gilos"));
+            executeEmployeeCreation(empId2, deptId1, new EmployeeName("Raul", "Watson"));
+            executeEmployeeCreation(empId3, deptId2, new EmployeeName("Ulric", "Tristan"));
+        }
+
+        @Test
+        public void ItShouldBeAbleToGetAllEmployeesWithASpecificDepartmentId() {
+            Employee e1 = repository.getEmployeeById(empId1);
+            Employee e2 = repository.getEmployeeById(empId2);
+            List<Employee> expected = Arrays.asList(e1, e2);
+
+            List<Employee> employees = repository.getAllEmployeesWithDepartmentIdOf(deptId1);
+            assertThat(employees, is(equalTo(expected)));
+        }
+
+        @Test
+        public void ItShouldBeAbleToGetAllEmployeesWithAnotherDepartmentId() {
+            Employee e1 = repository.getEmployeeById(empId3);
+            List<Employee> expected = Arrays.asList(e1);
+
+            List<Employee> employees = repository.getAllEmployeesWithDepartmentIdOf(deptId2);
+            assertThat(employees, is(equalTo(expected)));
+        }
+
+        @Test
+        public void ItShouldBeAbleToGetAllEmployees() {
+            Employee e1 = repository.getEmployeeById(empId1);
+            Employee e2 = repository.getEmployeeById(empId2);
+            Employee e3 = repository.getEmployeeById(empId3);
+            List<Employee> expected = new ArrayList<>();
+            expected.add(e1);
+            expected.add(e2);
+            expected.add(e3);
+
+            List<Employee> employees = repository.getAllEmployees();
+
+            assertThat(employees, is(equalTo(expected)));
+        }
+    }
+
+    public class ChangeEmployeeContext {
+
+        int empId = 6;
+        int deptId = 7;
+        String firstName = "Gian Carlo";
+        String lastName = "Gilos";
+
+        @Before
+        public void beforeEach() {
+            executeEmployeeCreation(empId, deptId, new EmployeeName(firstName, lastName));
+        }
+
+        @Test
+        public void ItShouldBeAbleToChangeTheFirstNameOfAnEmployee() {
+            String changeFirstName = "Pofay";
+            ChangeEmployee ce = new ChangeEmployeeName(repository, empId,
+                    new EmployeeName(changeFirstName, lastName));
+
+            ce.execute();
+
+            String expected = "Pofay Gilos";
+            Employee e = repository.getEmployeeById(empId);
+            assertThat(e.getName(), is(equalTo(expected)));
+        }
+
+        @Test
+        public void ItShouldBeAbleToChangeTheDepartmentIdOfAnEmployee() {
+            executeEmployeeCreation(empId, deptId, new EmployeeName(firstName, lastName));
+
+            int changedDeptId = 9;
+            ChangeEmployee ce = new ChangeEmployeeDepartmentId(repository,
+                    empId, changedDeptId);
+
+            ce.execute();
+
+            Employee e = repository.getEmployeeById(empId);
+            assertThat(e.getDeptId(), is(equalTo(changedDeptId)));
+        }
+
+        @Test
+        public void ItShouldBeAbleToChangeTheLastNameOfAnEmployee() {
+            executeEmployeeCreation(empId, deptId, new EmployeeName(firstName, lastName));
+
+            String changedLastName = "Tumulak";
+            ChangeEmployee ce = new ChangeEmployeeName(repository, empId,
+                    new EmployeeName(firstName, changedLastName));
+
+            ce.execute();
+
+            Employee e = repository.getEmployeeById(empId);
+            assertThat(e.getName(), is("Gian Carlo Tumulak"));
+        }
+
+    }
+
+    private void executeEmployeeCreation(final int empId, int deptId, EmployeeName name) {
+        CreateEmployee ce = new CreateEmployee(repository, empId, deptId, name);
+        ce.execute();
+    }
+
 }
