c981d02c2c3fd9e840ee5fe62a2bd445f045e1bc
Gian Carlo I. Gilos <gianCarlo.gilos@outlook.com>
2016-06-20 17:15:00 +0800

    We added new test to protect invariants of classes

---
+++ src/test/java/com/outlook/giancarlo/usecasetests/InvariantsTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.outlook.giancarlo.usecasetests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pofay
+ */
+public class InvariantsTest {
+    
+    public InvariantsTest() {
+    }
+    
+}
