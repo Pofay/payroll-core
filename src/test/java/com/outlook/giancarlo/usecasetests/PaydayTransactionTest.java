2934e4ac9a0714f18e83120a72bc5f333da2a4d8
Gian Carlo I. Gilos <gianCarlo.gilos@outlook.com>
2016-07-12 21:40:45 +0800

    We refactored test to instead use the added timecard from PostTimecard command

--- src/test/java/com/outlook/giancarlo/usecasetests/PaydayTransactionTest.java
+++ src/test/java/com/outlook/giancarlo/usecasetests/PaydayTransactionTest.java
@@ -8,36 +8,73 @@ package com.outlook.giancarlo.usecasetests;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.Arrays;
+import java.util.Collection;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  *
  * @author pofay
  */
+@RunWith(Parameterized.class)
 public class PaydayTransactionTest {
 
     public static double DELTA = 1.0;
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {LocalDate.of(2016, Month.JULY, 8), LocalTime.of(7, 30), LocalTime.of(16, 30), 122.0}});
+    }
+
+    private LocalDate payDate;
+    private LocalTime clockIn;
+    private LocalTime clockOut;
+    private double expected;
+    InMemoryPayrollRepository repository = new InMemoryPayrollRepository();
+
+    public PaydayTransactionTest(LocalDate payDate, LocalTime clockIn, LocalTime clockOut, double expected) {
+        this.payDate = payDate;
+        this.clockIn = clockIn;
+        this.clockOut = clockOut;
+        this.expected = expected;
+    }
+
     @Test
     public void ItShouldBeAbleToPayAHourlyEmployeeOnCorrectPayDate() {
-        InMemoryPayrollRepository repository = new InMemoryPayrollRepository();
-        EmployeeName name = new EmployeeName("Gian Carlo", "Gilos");
         int empId = 2;
+        createHourlyEmployee();
+        postTimecardTo(empId, payDate);
+        Timecard timecard = getTimecardOf(empId, payDate);
+        
+        timecard.clockIn(clockIn);
+        timecard.clockOut(clockOut);
+
+        PaydayTransaction t = new PaydayTransaction(payDate);
+        t.execute();
+
+        Paycheck paycheck = t.getPaycheckOf(empId);
+        assertEquals(expected, paycheck.grosspay, DELTA);
+    }
+
+    private void postTimecardTo(int empId, LocalDate date) {
+        PostTimecard postTimecard = new PostTimecard(repository, empId, date);
+        postTimecard.execute();
+    }
+
+    private void createHourlyEmployee() {
+        EmployeeName name = new EmployeeName("Gian Carlo", "Gilos");
         CreateHourlyEmployee che = new CreateHourlyEmployee(repository, 2, 3, name, 15.25);
         che.execute();
-        LocalDate paydate = LocalDate.of(2016, Month.JULY, 8);
-        PostTimecard postTimecard = new PostTimecard(repository, 2, paydate);
-        postTimecard.execute();
-        HourlyClassification hc = repository.getEmployeeById(empId).getClassification();
-        Timecard timecard = new Timecard(paydate);
-        hc.addTimecard(timecard);
-        timecard.clockIn(LocalTime.of(7, 30));
-        timecard.clockOut(LocalTime.of(16, 30));
-        
-        PaydayTransaction t = new PaydayTransaction(paydate);
-        t.execute();
-        
-        Paycheck paycheck = t.getPaycheckOf(empId);
-        assertEquals(122.0, paycheck.grosspay, DELTA);
     }
+
+    private Timecard getTimecardOf(int empId, LocalDate payDate) {
+        Employee e = repository.getEmployeeById(empId);
+        HourlyClassification hc = e.getClassification();
+        return hc.getTimecardIssuedOn(payDate);
+   }
 }
